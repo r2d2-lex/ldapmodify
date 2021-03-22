fa86547c1d10295595c8ed7a107b48733d1c2c63
suser <r2d2-lex@yandex.ru>
2021-03-22 16:56:59 +0500

    open_database, get_pgdb_user_room

--- modify.py
+++ modify.py
@@ -1,5 +1,7 @@
-import ldap
 import config
+import ldap
+import psycopg2
+from contextlib import closing
 from datetime import datetime, timedelta
 
 
@@ -136,7 +138,7 @@ def datetime_to_str(date_time):
 
 def main():
     all_users_dict = {}
-    user_attrs = 'lastLogonTimestamp', 'mail'
+    user_attrs = 'lastLogonTimestamp', 'physicalDeliveryOfficeName'
 
     lc = LdapModify(config.HOSTNAME, config.USERNAME, config.PASSWORD)
     for group_ou, group_description in lc.get_groups:
@@ -146,10 +148,37 @@ def main():
         all_users_dict = {**all_users_dict, **users_dict}
 
     print('Всего {} пользователей\r\n'.format(len(all_users_dict)))
-    get_users_info('results.txt', all_users_dict)
+
+    # Connect to PG_Base
+    with closing(open_database()) as connect:
+        with connect.cursor() as db_cursor:
+            get_users_info('results.txt', all_users_dict, db_cursor)
 
 
-def get_users_info(filename, users_dict):
+def open_database():
+    con = psycopg2.connect(
+        database=config.PG_DATABASE,
+        user=config.PG_USER,
+        password=config.PG_PASSWORD,
+        host=config.PG_HOST,
+        port=config.PG_PORT,
+    )
+    print("Database opened successfully")
+    return con
+
+
+def get_pgdb_user_room(cursor, login):
+    cursor.execute('SELECT room FROM users WHERE login LIKE \'{}\' LIMIT 1;'.format(login))
+    try:
+        room = cursor.fetchone()[0]
+    except (IndexError, TypeError):
+        return ''
+    if not room:
+        return ''
+    return room
+
+
+def get_users_info(filename, users_dict, cursor):
     with open(filename) as fh:
         login_names = fh.read().splitlines()
     for login in login_names:
@@ -159,7 +188,8 @@ def get_users_info(filename, users_dict):
             for key, value in user_record.items():
                 if key == 'lastLogonTimestamp':
                     value = datetime_to_str(value)
-                result_string += '{} '.format(value)
+                result_string += '{};'.format(value)
+            result_string += '{};'.format(get_pgdb_user_room(cursor, login))
             print(result_string)
         else:
             print('*** Нет записи для логина: "{}" в словаре!'.format(login))
